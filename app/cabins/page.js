a9dfc2d6b1391ba4c1df83f1385289124e83fa26
ksuthar937 <ksuthar937@gmail.com>
2024-07-03 19:44:41 +0530

    first commit

---
+++ app/cabins/page.js
@@ -0,0 +1,17 @@
+import Counter from "../components/Counter";
+
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return (
+    <div>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+      <Counter users={data} />
+    </div>
+  );
+}
